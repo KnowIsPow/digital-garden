918ac8426999c938b7ef91aff1425d89e7e54c21
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2021-12-22 14:22:57 -0500

    Add image headers to individual blog posts

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,5 +1,6 @@
 import { getPosts, getSinglePost } from 'data/posts';
 import Page from 'features/page';
+import { ImageHeader } from 'ui/header/image-header';
 import { TitleWithDescription } from 'ui/header/title-with-description';
 import { Container } from 'ui/layout/container';
 
@@ -39,10 +40,29 @@ export default function PostPage({ post }) {
       canonical={'/blog/' + post.slug}
       previewImage={post.feature_image}
     >
+      <ImageHeader
+        imageUrl={post.feature_image}
+        imageAlt={post.title}
+        overlayBg="bg-gradient-to-b from-transparent to-black opacity-80"
+      >
+        <div className="relative py-24 sm:py-32 content text-center">
+          <h1 className="text-white">{post.title}</h1>
+          <div className="mt-2 flex space-x-1 text-lg text-gray-200 justify-center">
+            <time dateTime={post.published_at}>
+              {Intl.DateTimeFormat('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              }).format(new Date(post.published_at))}
+            </time>
+            <span aria-hidden="true">&middot;</span>
+            <span>{post.reading_time} minute read</span>
+          </div>
+        </div>
+      </ImageHeader>
       <Container>
-        <TitleWithDescription title={post.title} description={post.custom_excerpt} center />
         <article
-          className="prose prose-blue mx-auto py-12"
+          className="prose prose-blue mx-auto"
           dangerouslySetInnerHTML={{ __html: post.html }}
         />
       </Container>
