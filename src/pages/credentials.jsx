59b5f1295f05738cf8503785ef6d5d318d7aea7f
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2022-01-02 00:08:39 -0500

    Update WorkExperience design

--- src/pages/credentials.jsx
+++ src/pages/credentials.jsx
@@ -17,7 +17,7 @@ export default function CredentialsPage({}) {
           description="It's important to consider the believability of resources. I'm going to share a lot of resources and experiences on this website, but I'm not an expert on everything I share. This page serves to highlight which resources are backed by experience, and which are my own opinion and should be taken with a grain of salt."
           wide
         />
-        <div className="space-y-4 mt-6">
+        <div className="mt-6 space-y-4 md:space-y-6">
           {workExperience.map((experience) => (
             <WorkExperience
               key={experience.company + '_' + experience.startDate}
@@ -32,16 +32,18 @@ export default function CredentialsPage({}) {
 
 function WorkExperience({ experience }) {
   return (
-    <div className="p-4 shadow bg-white">
-      <div className="flex justify-between items-center flex-wrap">
+    <div className="p-4 border md:p-6">
+      <div className="flex flex-wrap items-center justify-between">
         <h2>{experience.role}</h2>
         <div className="text-gray-400">
           {experience.startDate} – {experience.endDate}
         </div>
       </div>
 
-      <div>{experience.company}</div>
-      <div className="text-gray-400">{experience.location}</div>
+      <div className="flex flex-wrap items-center justify-between">
+        <div>{experience.company}</div>
+        <div className="text-gray-400">{experience.location}</div>
+      </div>
       <p className="mt-4">{experience.description}</p>
       {experience.accomplishments && <BulletedList items={experience.accomplishments} />}
     </div>
