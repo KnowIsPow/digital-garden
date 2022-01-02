0dced0aef6e07bbfcfb48637516c127eb2400772
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2022-01-01 23:55:31 -0500

    Whoops, happens. (forgot to remove placeholder text)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,19 +20,43 @@ export default function IndexPage() {
             description="Welcome to my digital garden 🌱 It's all about growing excitement, learning tech, and sharing the resources and lessons I've discovered along the way."
           />
         </div>
-        <section className="grid gap-6 py-6 lg:grid-cols-3">
+        <section className="grid gap-3 py-6 md:gap-6 lg:grid-cols-3">
           {[
-            { title: 'Values', href: '/about/#values' },
-            { title: 'Goals', href: '/about/#goals' },
-            { title: 'Experiences', href: '/credentials' },
-            { title: 'Blog', href: '/blog' },
-            { title: 'Books', href: '/resources' },
-            { title: 'Contact', href: '/contact' },
-          ].map(({ title, href }) => {
+            {
+              title: 'Values',
+              href: '/about/#values',
+              description: 'The principles that guide my decision making',
+            },
+            {
+              title: 'Goals',
+              href: '/about/#goals',
+              description: "What I'm constantly working towards",
+            },
+            {
+              title: 'Experiences',
+              href: '/credentials',
+              description: "The skills and wisdom I've aquired through work",
+            },
+            {
+              title: 'Blog',
+              href: '/blog',
+              description: 'A special place for my thoughts and opinions',
+            },
+            {
+              title: 'Books',
+              href: '/resources',
+              description: "The books I've read and rated for your enjoyment",
+            },
+            {
+              title: 'Contact',
+              href: '/contact',
+              description: "Share a story, an idea, or something you're proud of :)",
+            },
+          ].map(({ title, href, description }) => {
             return (
               <Link href={href} className="p-3 duration-300 border hover:border-primary">
                 <div className="font-semibold text-primary">{title}</div>
-                <p>This is just a test of a description</p>
+                <p>{description}</p>
               </Link>
             );
           })}
