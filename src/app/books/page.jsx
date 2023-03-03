40157c508c31e2d2f11d3a39c7ddc58ba62106cd
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2023-03-03 01:25:00 -0500

    Add book reflections

--- src/app/books/page.jsx
+++ src/app/books/page.jsx
@@ -10,17 +10,19 @@ export default async function BooksPage({ params }) {
         </div>
         <h1 className="mb-4 text-2xl font-bold tracking-tight sm:text-4xl">Book Recommendations</h1>
         <p className="text-gray-500">
-          A carefully curated (and priority ordered) selection of my top book recommendations. It's
-          a short list for now, but soon I'll categorize these by learning goals and rank each book
-          within its category.
+          A carefully curated (and priority ordered) selection of my top book recommendations.
+          It&apos;s a short list for now, but soon I&apos;ll add summaries for each of them.
         </p>
       </header>
 
-      <ul className="space-y-2">
+      <ul className="space-y-4">
         {books
           .sort((a, b) => b.score - a.score)
           .map((book) => (
-            <li key={book.title}>{`${book.title} – ⭐ ${book.score}`}</li>
+            <li key={book.title}>
+              <h2>{`${book.title} – ⭐ ${book.score}`}</h2>
+              <p className="text-gray-600">{book.keyTakeaway}</p>
+            </li>
           ))}
       </ul>
     </>
