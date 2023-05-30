1e73092af68236d156ab1731c2701bd08b4b8283
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-05-30 14:19:25 +0700

    Add books

--- src/data/books.js
+++ src/data/books.js
@@ -1,150 +1,213 @@
 export const books = [
-  { title: 'Meditations', score: 10, keyTakeaway: 'A semi-stoic guide to become a better human.' },
   {
-    title: 'Subtle Art of Not Giving A F*ck',
+    title: "Meditations",
     score: 10,
-    keyTakeaway: "Learn to prioritize what's important and be indiferrent to the rest.",
+    keyTakeaway: "A semi-stoic guide to become a better human.",
   },
   {
-    title: 'How to Make Friends & Influence People',
+    title: "Subtle Art of Not Giving A F*ck",
+    score: 10,
+    keyTakeaway:
+      "Learn to prioritize what's important and be indiferrent to the rest.",
+  },
+  {
+    title: "Man's Search for Meaning",
     score: 9,
-    keyTakeaway: 'The book that started my personal growth journey.',
+    keyTakeaway: "Endure life with an awesome sense of purpose.",
   },
-  { title: 'As a Man Thinketh', score: 9, keyTakeaway: 'Find inner peace and happiness.' },
   {
-    title: 'Extreme Ownership',
+    title: "As a Man Thinketh",
+    score: 9,
+    keyTakeaway: "Find inner peace and happiness.",
+  },
+  {
+    title: "Extreme Ownership",
     score: 8.5,
-    keyTakeaway: 'Improve your life by taking accountability for everything in it.',
+    keyTakeaway:
+      "Improve your life by taking accountability for everything in it.",
   },
   {
-    title: 'The Body Keeps the Score',
+    title: "How to Make Friends & Influence People",
     score: 8,
-    keyTakeaway: 'A well-researched analysis of trauma and modern approaches for addressing it.',
+    keyTakeaway: "The book that started my personal growth journey.",
   },
-  { title: 'Obstacle is the Way', score: 8, keyTakeaway: 'A modern take on ancient stoicism.' },
-  { title: 'Sapiens', score: 8, keyTakeaway: 'The history of humanity.' },
-  { title: 'Rework', score: 6, keyTakeaway: '' },
-  { title: 'The Alchemist', score: 8, keyTakeaway: 'A story about finding your personal legend.' },
-  { title: 'Siddhartha', score: 8, keyTakeaway: 'A story of suffering and the path to nirvana.' },
+  {
+    title: "The Body Keeps the Score",
+    score: 8,
+    keyTakeaway:
+      "A well-researched analysis of trauma and modern approaches for addressing it.",
+  },
+  {
+    title: "Obstacle is the Way",
+    score: 8,
+    keyTakeaway: "A modern take on ancient stoicism.",
+  },
+  { title: "Sapiens", score: 8, keyTakeaway: "The history of humanity." },
+  { title: "Rework", score: 6, keyTakeaway: "" },
+  {
+    title: "The Alchemist",
+    score: 8,
+    keyTakeaway: "A story about finding your personal legend.",
+  },
+  {
+    title: "Siddhartha",
+    score: 8,
+    keyTakeaway: "A story of suffering and the path to nirvana.",
+  },
   {
     title: "Can't Hurt Me",
     score: 8.5,
     keyTakeaway:
-      'There are many mental limits that hold us back. When we push past these, the outcomes can be incredible.',
+      "There are many mental limits that hold us back. When we push past these, the outcomes can be incredible.",
   },
-  { title: 'Four Agreements', score: 8.5, keyTakeaway: 'Find inner peace.' },
-  { title: 'Mastery of Love', score: 8.5, keyTakeaway: 'Practive wordly love.' },
+  { title: "Four Agreements", score: 8.5, keyTakeaway: "Find inner peace." },
   {
-    title: 'The Power of Vulnerability',
+    title: "Mastery of Love",
+    score: 8.5,
+    keyTakeaway: "Practive wordly love.",
+  },
+  {
+    title: "The Power of Vulnerability",
     score: 8.5,
     keyTakeaway:
-      'Lean into vulnerability and release yourself from a lifetime of shame and suffering.',
+      "Lean into vulnerability and release yourself from a lifetime of shame and suffering.",
   },
   {
-    title: 'The Enchiridion',
+    title: "The Enchiridion",
     score: 8,
-    keyTakeaway: 'A summary of the stoic teachings of Epictetus.',
+    keyTakeaway: "A summary of the stoic teachings of Epictetus.",
   },
   {
-    title: 'Discourses of Epictetus',
+    title: "Discourses of Epictetus",
     score: 8,
-    keyTakeaway: 'Learn about stoicism and how it can help you become a better human.',
+    keyTakeaway:
+      "Learn about stoicism and how it can help you become a better human.",
   },
   {
-    title: "Man's Search for Meaning",
-    score: 8,
-    keyTakeaway: 'Endure life with an awesome sense of purpose.',
-  },
-  {
-    title: 'The Second Mountain',
+    title: "The Second Mountain",
     score: 7.5,
-    keyTakeaway: 'Live a life of commitment in relationship to others.',
+    keyTakeaway: "Live a life of commitment in relationship to others.",
   },
   {
-    title: 'When Breath Becomes Air',
+    title: "When Breath Becomes Air",
     score: 7.5,
-    keyTakeaway: 'A true story about a man who faced death with integrity. This book made me cry.',
-  },
-  { title: 'Think Like A Monk', score: 7, keyTakeaway: 'A reflection of the monk lifestyle.' },
-  { title: '12 Rules for Life', score: 7, keyTakeaway: 'As the title says.' },
-  {
-    title: 'Cultivating an Unshakeable Character',
-    score: 7,
-    keyTakeaway: 'Reflect on the components of your character.',
-  },
-  {
-    title: 'How Will You Measure Your Life?',
-    score: 7,
     keyTakeaway:
-      'Thoughtfully consider what you most value in life and embody those values within your career, personal life, and family dynamics.',
+      "A true story about a man who faced death with integrity. This book made me cry.",
   },
   {
-    title: 'Excellent Sheep',
+    title: "Think Like A Monk",
+    score: 7,
+    keyTakeaway: "A reflection of the monk lifestyle.",
+  },
+  { title: "12 Rules for Life", score: 7, keyTakeaway: "As the title says." },
+  {
+    title: "The Elegance of The Hedgehog",
     score: 7,
     keyTakeaway:
-      'The elite educational system has created a generation of individuals who do things not because they are virtuous, but because they are necessary (and this is bad because we think critically about ourselves and our values less often).',
+      "A beautiful story about life, meaning, and life's trivialities.",
   },
   {
-    title: 'Influence: Science and Practice',
+    title: "Cultivating an Unshakeable Character",
     score: 7,
-    keyTakeaway: 'Learn how to market to anyone.',
+    keyTakeaway: "Reflect on the components of your character.",
   },
   {
-    title: '48 Laws of Power',
+    title: "How Will You Measure Your Life?",
     score: 7,
     keyTakeaway:
-      'This book hurt my soul to read because it focuses primarily on politics and manipulation, but the information is important for understanding that part of humanity.',
+      "Thoughtfully consider what you most value in life and embody those values within your career, personal life, and family dynamics.",
   },
-  { title: 'Sex At Dawn', score: 7, keyTakeaway: 'Learn about human mating behaviors.' },
-  { title: 'Purple Cow', score: 6, keyTakeaway: 'An essential guide to value-driven marketing.' },
   {
-    title: 'The Lean Startup',
+    title: "Excellent Sheep",
+    score: 7,
+    keyTakeaway:
+      "The elite educational system has created a generation of individuals who do things not because they are virtuous, but because they are necessary (and this is bad because we think critically about ourselves and our values less often).",
+  },
+  {
+    title: "The E-Myth: Revisited",
+    score: 7,
+    keyTakeaway:
+      "Systemization in critically important in business and in life.",
+  },
+  {
+    title: "Influence: Science and Practice",
+    score: 7,
+    keyTakeaway: "Learn how to market to anyone.",
+  },
+  {
+    title: "48 Laws of Power",
+    score: 7,
+    keyTakeaway:
+      "This book hurt my soul to read because it focuses primarily on politics and manipulation, but the information is important for understanding that part of humanity.",
+  },
+  {
+    title: "Sex At Dawn",
+    score: 7,
+    keyTakeaway: "Learn about human mating behaviors.",
+  },
+  {
+    title: "Purple Cow",
     score: 6,
-    keyTakeaway: 'Learn how to run a startup in the modern age.',
+    keyTakeaway: "An essential guide to value-driven marketing.",
   },
   {
-    title: 'Never Split The Difference',
+    title: "The Lean Startup",
     score: 6,
-    keyTakeaway: 'Learn to negotiate with empathy, and expand the pool of resources for everyone.',
+    keyTakeaway: "Learn how to run a startup in the modern age.",
   },
   {
-    title: 'The Art of War',
-    score: 6,
-    keyTakeaway: 'A collection of ancient war reflections that you can apply to your modern life.',
-  },
-  { title: 'The 4 Hour Work Week', score: 6, keyTakeaway: '' },
-  { title: 'High Performance Habits', score: 6, keyTakeaway: '' },
-  { title: 'The 7 Habits of Highly Effective People', score: 6, keyTakeaway: '' },
-  { title: 'Stillness Is the Key', score: 6, keyTakeaway: '' },
-  { title: 'The Surrender Experiment', score: 6, keyTakeaway: '' },
-  { title: 'The Prince', score: 6, keyTakeaway: 'A guide for action in a political environment.' },
-  { title: 'Principles', score: 6, keyTakeaway: '' },
-  {
-    title: 'The State of Affairs',
-    score: 6,
-    keyTakeaway: 'Processing infidelity within a relationship.',
-  },
-  {
-    title: 'Crucial Conversations',
+    title: "Never Split The Difference",
     score: 6,
     keyTakeaway:
-      'Practicing empathy, minimizing negative storytelling, and becoming an effective communicator.',
+      "Learn to negotiate with empathy, and expand the pool of resources for everyone.",
   },
-  { title: 'Ego Is The Enemy', score: 6, keyTakeaway: '' },
   {
-    title: 'The Way of the Superior Man',
+    title: "The Art of War",
     score: 6,
     keyTakeaway:
-      'Lean into life with love and passionatly pursue your ultimate purpose at all costs.',
+      "A collection of ancient war reflections that you can apply to your modern life.",
+  },
+  { title: "The 4 Hour Work Week", score: 6, keyTakeaway: "" },
+  { title: "High Performance Habits", score: 6, keyTakeaway: "" },
+  {
+    title: "The 7 Habits of Highly Effective People",
+    score: 6,
+    keyTakeaway: "",
+  },
+  { title: "Stillness Is the Key", score: 6, keyTakeaway: "" },
+  { title: "The Surrender Experiment", score: 6, keyTakeaway: "" },
+  {
+    title: "The Prince",
+    score: 6,
+    keyTakeaway: "A guide for action in a political environment.",
+  },
+  { title: "Principles", score: 6, keyTakeaway: "" },
+  {
+    title: "The State of Affairs",
+    score: 6,
+    keyTakeaway: "Processing infidelity within a relationship.",
+  },
+  {
+    title: "Crucial Conversations",
+    score: 6,
+    keyTakeaway:
+      "Practicing empathy, minimizing negative storytelling, and becoming an effective communicator.",
+  },
+  { title: "Ego Is The Enemy", score: 6, keyTakeaway: "" },
+  {
+    title: "The Way of the Superior Man",
+    score: 6,
+    keyTakeaway:
+      "Lean into life with love and passionatly pursue your ultimate purpose at all costs.",
   },
 
-  { title: 'Rich Dad Poor Dad', score: 5, keyTakeaway: '' },
-  { title: 'The Effective Executive', score: 5, keyTakeaway: '' },
-  { title: "You're a Badass at Making Money", score: 5, keyTakeaway: '' },
-  { title: 'The Great Mental Models', score: 5, keyTakeaway: '' },
-  { title: 'Models (Mark Manson)', score: 5, keyTakeaway: '' },
-  { title: 'The $100 Startup', score: 4, keyTakeaway: '' },
-  { title: 'Unfu*k Yourself', score: 4, keyTakeaway: '' },
-  { title: 'The Go-Giver', score: 4, keyTakeaway: '' },
-  { title: 'Start With Why', score: 3, keyTakeaway: '' },
+  { title: "Rich Dad Poor Dad", score: 5, keyTakeaway: "" },
+  { title: "The Effective Executive", score: 5, keyTakeaway: "" },
+  { title: "You're a Badass at Making Money", score: 5, keyTakeaway: "" },
+  { title: "The Great Mental Models", score: 5, keyTakeaway: "" },
+  { title: "Models (Mark Manson)", score: 5, keyTakeaway: "" },
+  { title: "The $100 Startup", score: 4, keyTakeaway: "" },
+  { title: "Unfu*k Yourself", score: 4, keyTakeaway: "" },
+  { title: "The Go-Giver", score: 4, keyTakeaway: "" },
+  { title: "Start With Why", score: 3, keyTakeaway: "" },
 ];
