14f5654752a5a57c942c3676ac9a85723eb90e71
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-06-26 21:44:49 +0700

    Add project page

---
+++ src/app/(sub-pages)/projects/page.jsx
@@ -0,0 +1,89 @@
+import { BackToMainPage } from "@/components/back-button";
+import { books } from "@/data/books";
+import { projects } from "@/data/projects";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Ongoing Projects",
+  description:
+    "Here's what I am currently working on as big projects in my life.",
+  openGraph: {
+    title: "Ongoing Projects - Curtis Fisher",
+    description:
+      "Here's what I am currently working on as big projects in my life.",
+    url: "https://www.curtisfisher.com/projects",
+  },
+  alternates: {
+    canonical: `/projects`,
+  },
+};
+
+export default async function ProjectsPage() {
+  return (
+    <>
+      <header className="pb-10 mb-10 border-b border-gray-200 sm:mb-16 sm:pb-16">
+        <div className="mb-6">
+          <BackToMainPage />
+        </div>
+        <h1 className="mb-4 text-2xl font-bold tracking-tight text-blue-700 lowercase sm:text-4xl">
+          Ongoing Projects.
+        </h1>
+        <p className="text-gray-500">
+          Public accountability is a great motivator. Here is a list of what I'm
+          working on with a candid reflection of how well I&apos;m executing
+          against my goals.
+        </p>
+      </header>
+
+      <ul className="space-y-12">
+        {projects.map((project) => (
+          <Project key={project.name} project={project} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Project({ project }) {
+  return (
+    <li>
+      <h2
+        className={`text-blue-700 text-lg ${project.previewImage && "hidden"}`}
+      >
+        {project.name}
+      </h2>
+      {project.previewImage && (
+        <Image
+          src={project.previewImage}
+          className="w-auto h-12 mb-4"
+          width={250}
+          height={250}
+        />
+      )}
+      <p className="text-gray-500">Started {project.startDate}</p>
+      <p className="mt-4">{project.description}</p>
+      {project.objectives && (
+        <>
+          <div className="mt-4 font-medium">Key Objectives</div>
+          <ul className="ml-4 space-y-2 mt-4">
+            {project.objectives.map((objective) => (
+              <li key={objective}>❗ {objective}</li>
+            ))}
+          </ul>
+        </>
+      )}
+      {project.href && (
+        <div className="mt-4">
+          <Link
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            href={project.href}
+          >
+            View Project
+          </Link>
+        </div>
+      )}
+    </li>
+  );
+}
---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    name: "GrowthSuite",
+    startDate: "Oct 2019",
+    description: `A cutting-edge PropTech consulting and data services company.`,
+    href: "https://growthsuite.io",
+    objectives: [
+      "Become an industry-leader in property technology innovation.",
+      "Empower brokerages of any-size to utilize data for effective decision making.",
+    ],
+    previewImage: "/images/projects/growthsuite.svg",
+  },
+  {
+    name: "Sound Data Solutions",
+    startDate: "May 2023",
+    description: `Local Web Design, SEO, and Analytics Services for Martha's Vineyard.`,
+    href: "https://sounddatasolutions.com/",
+    previewImage: "/images/projects/sound-data-solutions.svg",
+  },
+  {
+    name: "Martha's Visit",
+    startDate: "May 2021",
+    description: `Vacation planning services for Martha's Vineyard.`,
+    href: "https://www.marthasvisit.com/",
+    previewImage: "/images/projects/marthasvisit.svg",
+  },
+];
