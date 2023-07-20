b4313afe1223e7550657c7e9cb044d2a1a5e7138
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-07-20 23:41:52 +0900

    Adjust mobile opacity of recent articles

--- src/app/blog/[slug]/layout.jsx
+++ src/app/blog/[slug]/layout.jsx
@@ -1,6 +1,6 @@
-import { ArticleCard } from '@/components/article-card';
-import { getArticles } from '@/functions/markdown';
-import Link from 'next/link';
+import { ArticleCard } from "@/components/article-card";
+import { getArticles } from "@/functions/markdown";
+import Link from "next/link";
 
 export default async function ArticleLayout({ params, children }) {
   const articles = await getArticles();
@@ -9,10 +9,10 @@ export default async function ArticleLayout({ params, children }) {
     <div className="relative py-32 sm:py-48 lg:py-56">
       {children}
       <div className="pt-16 mt-16 border-t border-gray-200 group/recent">
-        <h2 className="mb-6 text-3xl font-bold tracking-tight text-blue-700 lowercase duration-200 sm:text-4xl opacity-60 group-hover/recent:opacity-100">
+        <h2 className="mb-6 text-3xl font-bold tracking-tight text-blue-700 lowercase duration-200 sm:text-4xl sm:opacity-60 group-hover/recent:opacity-100">
           Recent Articles.
         </h2>
-        <div className="space-y-12 duration-100 opacity-60 group-hover/recent:opacity-100">
+        <div className="space-y-12 duration-100 sm:opacity-60 group-hover/recent:opacity-100">
           {articles
             .filter((article) => article.meta.slug !== params.slug)
             .sort((a, b) => new Date(b.meta.date) - new Date(a.meta.date))
