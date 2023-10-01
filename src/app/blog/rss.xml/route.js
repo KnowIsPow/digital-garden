c92a1498a74abad8be150f1284012ff45e5e65ac
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-10-01 12:47:37 -0400

    Add RSS feed

---
+++ src/app/blog/rss.xml/route.js
@@ -0,0 +1,33 @@
+import { getArticles } from "@/functions/markdown";
+import Rss from "rss";
+
+const SITE_URL = process.env.SITE_URL || "https://www.curtisfisher.com";
+
+export async function GET() {
+  const articles = await getArticles();
+
+  const feed = new Rss({
+    title: "Curtis Fisher's Blog",
+    description:
+      "A selection of thoughts, ideas, and reflections to delight the introspective spirit.",
+    feed_url: `${SITE_URL}/blog/rss.xml`,
+    site_url: SITE_URL,
+    language: "en",
+  });
+
+  articles.forEach((article) => {
+    feed.item({
+      title: article.meta.title,
+      description: article.meta.description,
+      url: `${SITE_URL}/blog/${article.meta.slug}`,
+      guid: `${SITE_URL}/blog/${article.meta.slug}`,
+      date: article.meta.updatedAt,
+    });
+  });
+
+  return new Response(feed.xml(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
