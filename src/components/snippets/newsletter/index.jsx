9aac753d6e870877f9c9a8ebfb4a83348c22e0f7
KnowIsPow <KnowIsPow@users.noreply.github.com>
2024-03-09 15:08:23 -0500

    Add title to newsletter iframe for accessibility

--- src/components/snippets/newsletter/index.jsx
+++ src/components/snippets/newsletter/index.jsx
@@ -1,6 +1,7 @@
 export function NewsletterForm({}) {
   return (
     <iframe
+      title="Newsletter Subscription Form"
       src="https://embeds.beehiiv.com/426e1d3a-3fd0-4367-ad7e-c2711f9938f7"
       data-test-id="beehiiv-embed"
       width="100%"
