b986c737d24e57c069b8657f21f110ea4985c98e
KnowIsPow <KnowIsPow@users.noreply.github.com>
2024-03-09 16:48:23 -0500

    Adjust blog to show indicate articles that haven't been read, opposed to indicating articles that have been read.

--- src/components/article-card.jsx
+++ src/components/article-card.jsx
@@ -1,16 +1,20 @@
 import { ArticleTime } from "@/components/article-time";
 import Link from "next/link";
 import { Heading } from "./ui/heading";
+import {
+  CheckCircleIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "@heroicons/react/24/outline";
 
 export function ArticleCard({ article }) {
   return (
     <article key={article.title} className="relative group">
       <Link href={`/blog/${article.meta.slug}`} className="group/link">
-        <div className="absolute text-white group-visited/link:text-black dark:text-black dark:group-visited/link:text-white -translate-x-5">
-          x
-        </div>
-
         <div className="lowercase flex justify-between items-center">
+          <div className="absolute text-black group-visited/link:text-white dark:text-white dark:group-visited/link:text-black -translate-x-6">
+            <EyeSlashIcon className="w-4 h-4" />
+          </div>
           <Heading
             size="h3"
             className=" text-gray-900 dark:text-gray-50 group-hover:underline underline-offset-8"
