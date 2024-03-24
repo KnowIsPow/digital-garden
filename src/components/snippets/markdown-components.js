7327494a1b495cc740d744df58632dbfebc2185f
KnowIsPow <KnowIsPow@users.noreply.github.com>
2024-03-24 01:28:57 -0400

    Add pinterest sharing to blog images

--- src/components/snippets/markdown-components.js
+++ src/components/snippets/markdown-components.js
@@ -7,16 +7,20 @@ import WageCalculator from "@/components/snippets/calculators/wages";
 import MagicChecklist from "@/components/snippets/visualization/magic-checklist";
 import Calendar from "@/components/calendar/";
 import Wide from "@/components/ui/formatting/wide";
+import { PinterestShare } from "../ui/pinterest-share";
 
 export const markdownComponents = {
   img: (props) => (
-    <Image
-      {...props}
-      height={3000}
-      width={1500}
-      alt={props.alt}
-      className="my-0 w-full rounded-lg object-contain"
-    />
+    <span className="block relative group">
+      <Image
+        {...props}
+        height={3000}
+        width={1500}
+        alt={props.alt}
+        className="my-0 w-full rounded-lg object-contain"
+      />
+      <PinterestShare excerpt={props.alt} imageUrl={props.src} />
+    </span>
   ),
   a: ({ href, children }) => {
     // is an internal link
